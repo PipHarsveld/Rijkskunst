d63e285fa27539c5fbfed051a008efd849a33a56
Pip <pip.harsveld@hva.nl>
2023-05-26 14:29:50 +0200

    Manipulate data with .map

--- router/router.js
+++ router/router.js
@@ -11,13 +11,21 @@ router.get('/', (req, res) => {
 })
 
 router.get('/overview', (req, res) => {
-    request(`https://www.rijksmuseum.nl/api/nl/collection/?key=${process.env.API_KEY}&ps=10`, { json: true }, function (err, response, data) {
+    request(`https://www.rijksmuseum.nl/api/nl/collection/?key=${process.env.API_KEY}&ps=10&imgonly=True`, { json: true }, function (err, response, data) {
         if (err) {
             res.send(err);
             console.error('error:', err);
         } else {
             const artPieces = data.artObjects;
-            res.render('overview', { layout: 'index', data: artPieces, title: 'Overzicht' });
+            const transformedArtPieces = artPieces.map((artPiece) => {
+                return {
+                    id: artPiece.id,
+                    title: artPiece.title,
+                    image: artPiece.webImage.url
+                };
+            });
+
+            res.render('overview', { layout: 'index', data: transformedArtPieces, title: 'Overzicht' });
         }
     })
 });
@@ -26,36 +34,52 @@ router.get('/details/:id', (req, res) => {
     console.log("test params", req.params.id);
     const idWithPrefix = req.params.id;
     const id = idWithPrefix.replace('nl-', '');
-
+  
     request(`https://www.rijksmuseum.nl/api/nl/collection/${id}?key=${process.env.API_KEY}`, { json: true }, function (err, response, data) {
-        if (err) {
-            res.send(err);
-            console.error('error:', err);
-        } else {
-            const details = data.artObject;
-            res.render('details', { layout: 'index', data: details, title: 'Details' });
-        }
-    })
+      if (err) {
+        res.send(err);
+        console.error('error:', err);
+      } else {
+        const details = data.artObject;
+  
+        const transformedDetails = {
+          id: details.id,
+          title: details.title,
+          image: details.webImage.url,
+          info: details.description,
+          tags: details.objectTypes
+        };
+  
+        res.render('details', { layout: 'index', data: transformedDetails, title: 'Details' });
+      }
+    });
+  });
 
-});
 
-
-router.get('/categorie/:type', (req, res) => {
+  router.get('/categorie/:type', (req, res) => {
     const typeArt = req.params['type'];
-
-    request(`https://www.rijksmuseum.nl/api/nl/collection/?key=${process.env.API_KEY}&type=${typeArt}`,
-        { json: true },
-        function (err, response, data) {
-            if (err) {
-                res.send(err);
-                console.error('error:', err);
-            } else {
-                const specificArtPieces = data.artObjects;
-                res.render('category', { layout: 'index', data: specificArtPieces, categorie: typeArt, title: 'Categorie' });
-            }
+  
+    request(`https://www.rijksmuseum.nl/api/nl/collection/?key=${process.env.API_KEY}&type=${typeArt}&imgonly=True`,
+      { json: true },
+      function (err, response, data) {
+        if (err) {
+          res.send(err);
+          console.error('error:', err);
+        } else {
+          const specificArtPieces = data.artObjects;
+          const transformedArtPieces = specificArtPieces.map((artPiece) => {
+            return {
+                id: artPiece.id,
+                title: artPiece.title,
+                image: artPiece.webImage.url
+            };
+          });
+  
+          res.render('category', { layout: 'index', data: transformedArtPieces, categorie: typeArt, title: 'Categorie' });
         }
+      }
     )
-});
+  });
 
 
 router.get('/zoeken', (req, res) => {
@@ -65,19 +89,28 @@ router.get('/zoeken', (req, res) => {
 router.post('/zoekresultaten', async (req, res) => {
     const inputSearchTerm = req.body.zoeken;
     console.log("inputSearchTerm", inputSearchTerm);
-    request(`https://www.rijksmuseum.nl/api/nl/collection/?key=${process.env.API_KEY}&q=${inputSearchTerm}`,
-        { json: true },
-        function (err, response, data) {
-            if (err) {
-                res.send(err);
-                console.error('error:', err);
-            } else {
-                const specificArtPieces = data.artObjects;
-                res.render('search', { layout: 'index', data: specificArtPieces, title: 'Zoekresultaten' });
-            }
+    request(`https://www.rijksmuseum.nl/api/nl/collection/?key=${process.env.API_KEY}&q=${inputSearchTerm}&imgonly=True`,
+      { json: true },
+      function (err, response, data) {
+        if (err) {
+          res.send(err);
+          console.error('error:', err);
+        } else {
+          const specificArtPieces = data.artObjects;
+          const transformedArtPieces = specificArtPieces.map((artPiece) => {
+            return {
+                id: artPiece.id,
+                title: artPiece.title,
+                image: artPiece.webImage.url,
+                objectNumber: artPiece.objectNumber
+            };
+          });
+  
+          res.render('search', { layout: 'index', data: transformedArtPieces, title: 'Zoekresultaten' });
         }
+      }
     )
-});
+  });
 
 
 
